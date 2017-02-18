11e6ddfc6797cc373d46b1c83a50d8f1772314e9
Kyle Mellander <kmellander@focusvision.com>
2017-02-17 22:14:17 -0800

    Add docs for room model

--- web/models/room.ex
+++ web/models/room.ex
@@ -1,4 +1,11 @@
 defmodule Noegen.Room do
+  @moduledoc """
+  A room for containing a group of people to have messages specific to this
+  room.
+
+  Relationships:
+    - has_and_belongs_to_many Users
+  """
   use Noegen.Web, :model
 
   schema "rooms" do
@@ -11,6 +18,7 @@ defmodule Noegen.Room do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+  @spec changeset(struct, map) :: struct
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :topic])
