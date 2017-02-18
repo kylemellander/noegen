269ace0f285b162111e77e4dfe3b98c54616ab03
Kyle Mellander <kmellander@focusvision.com>
2017-02-17 22:09:45 -0800

    Add docs for UserRoom model

--- web/models/user_room.ex
+++ web/models/user_room.ex
@@ -1,4 +1,7 @@
 defmodule Noegen.UserRoom do
+  @moduledoc """
+    The join table between User and Room
+  """
   use Noegen.Web, :model
 
   schema "user_rooms" do
@@ -11,6 +14,7 @@ defmodule Noegen.UserRoom do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+  @spec changeset(struct, map) :: struct
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [])
