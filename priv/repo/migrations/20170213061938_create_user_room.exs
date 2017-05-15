d22e6a71ea7e20b7122d65366d7ee787c1932fd5
Kyle Mellander <kmellander@focusvision.com>
2017-05-15 08:00:56 -0700

    update migration for users and rooms join table

--- priv/repo/migrations/20170213061938_create_user_room.exs
+++ priv/repo/migrations/20170213061938_create_user_room.exs
@@ -1,15 +1,13 @@
-defmodule Noegen.Repo.Migrations.CreateUserRoom do
+defmodule Noegen.Repo.Migrations.CreateUsersRooms do
   use Ecto.Migration
 
   def change do
-    create table(:user_rooms) do
+    create table(:users_rooms) do
       add :user_id, references(:users, on_delete: :nothing), null: false
       add :room_id, references(:rooms, on_delete: :nothing), null: false
-
-      timestamps()
     end
-    create index(:user_rooms, [:user_id])
-    create index(:user_rooms, [:room_id])
-    create index(:user_rooms, [:user_id, :room_id], unique: true)
+    create index(:users_rooms, [:user_id])
+    create index(:users_rooms, [:room_id])
+    create index(:users_rooms, [:user_id, :room_id], unique: true)
   end
 end
